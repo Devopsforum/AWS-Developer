42007dda9f5b1d48bf3451e1ce056c6cc17b662b
DevOpsForum.info <40804538+Devopsforum@users.noreply.github.com>
2018-10-22 22:38:04 -0700

    Java Class for listing Bucket from AWS

---
+++ S3ListBuckets.java
@@ -0,0 +1,33 @@
+package SpotUrbeer.s3integration;
+
+
+import java.io.IOException;
+import java.util.List;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.Protocol;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+
+/**
+ * Hello world!
+ *
+ */
+public class S3ListBuckets {
+	public static void main(String[] args) throws IOException {
+		AWSCredentials credentials = new BasicAWSCredentials("AKIAJ2R4AWGKDMAF66NQ", "SeAQCq9KeIkYJCda5wNQ1nd7aVEfuGz7lh8W/XxH");
+
+		ClientConfiguration clientConfig = new ClientConfiguration();
+		clientConfig.setProtocol(Protocol.HTTP);
+
+		AmazonS3 conn = new AmazonS3Client(credentials, clientConfig);
+		List<Bucket> buckets =  conn.listBuckets();
+		for (Bucket bucket : buckets) {
+			System.out.println("Bucket Name"+bucket.getName());
+		}
+	}
+
+}
